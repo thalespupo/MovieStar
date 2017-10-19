bc1eecc7003e992bfd063a4ea7ace2d8ce320a2c
Thales Pupo Araújo <thalespupo@gmail.com>
2017-10-18 22:13:41 -0200

    Added databinding in MovieDetailsActivity
    Signed-off-by: Thales Pupo Araújo <thalespupo@gmail.com>

--- app/src/main/java/com/tapura/moviestar/details/MovieDetailsActivity.java
+++ app/src/main/java/com/tapura/moviestar/details/MovieDetailsActivity.java
@@ -1,15 +1,16 @@
 package com.tapura.moviestar.details;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.tapura.moviestar.R;
+import com.tapura.moviestar.databinding.ActivityMovieDetailsBinding;
 import com.tapura.moviestar.model.ResultsItem;
 
 import org.parceler.Parcels;
-import org.w3c.dom.Text;
 
 import static com.tapura.moviestar.Constants.KEY_MOVIE;
 
@@ -18,11 +19,14 @@ public class MovieDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_details);
+        ActivityMovieDetailsBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_movie_details);
+
         Intent intent = getIntent();
         ResultsItem movie = Parcels.unwrap(intent.getParcelableExtra(KEY_MOVIE));
-        TextView tv = findViewById(R.id.text_example);
-        tv.setText(movie.getOriginalTitle());
+
+        binding.setMovie(movie);
+
+
     }
 
 }
